9dfda98dd72a27c4016fd2ddae3d4c9fee05afd3
Minh Nguyen <24521065@gm.uit.edu.vn>
2026-03-25 10:57:37 +0700

    feat: UI for multiple choice and short/long answer

---
+++ components/survey-create/question/question-types/ShortAnswerSurveyQuestion.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input";
+
+export function ShortAnswerSurveyQuestion() {
+  return (
+    <div className="mt-2 w-1/2">
+      <Input disabled placeholder="Short answer text" className="border-dashed bg-gray-50/5" />
+    </div>
+  );
+}
