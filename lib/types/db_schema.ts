08a113bba8542e8cf723b490cb5833b09e663767
Thaiminh2022 <thaiminh2022@gmail.com>
2026-05-18 09:05:24 +0700

    update db schema to match with db

--- lib/types/db_schema.ts
+++ lib/types/db_schema.ts
@@ -12,6 +12,8 @@ export const surveyRowSchema = z.object({
   state: surveyStatusSchema,
   image: z.string().nullable(),
   description: z.string().nullable(),
+  submission_count: z.int().positive().nonoptional(),
+  view_count: z.int().positive().nonoptional(),
   created_at: z.coerce.date(),
 });
 
